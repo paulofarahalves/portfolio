0e60a9e0d0d748b7490d14965b0676997f077210
Paulo Farah <paulofarahalves@hotmail.com>
2023-02-09 21:10:14 -0300

    editing responsiveness

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
+function alertSubmit(e) {
+	e.preventDefault();
+	alert('Your message was successfully sent!');
+	form.reset();
+}
 
+const form = document.querySelector('form');
+form.addEventListener('submit', alertSubmit);
