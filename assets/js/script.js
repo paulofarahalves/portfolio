0eec4627e09b1505971193cd966dba6030cdbd67
Paulo Farah <paulofarahalves@hotmail.com>
2023-02-10 12:04:52 -0300

    editing menu toggle

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,20 @@ function alertSubmit(e) {
 const form = document.querySelector('form');
 form.addEventListener('submit', alertSubmit);
 */
+var menuIsOpened = false;
+const menuBtn = document.getElementById('header-menu-btn');
+const header = document.getElementById('header');
+
+menuBtn.addEventListener('click', () => {
+	toggleMenu();
+});
+
+const toggleMenu = () => {
+	if (menuIsOpened) {
+		header.style.display = 'none';
+		menuIsOpened = false;
+	} else {
+		header.style.display = 'flex';
+		menuIsOpened = true;
+	}
+};
